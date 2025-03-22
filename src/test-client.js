9106c947b68282fe4a15c53957cb643b571e2356
Ayndrome <Shashanksaket9@gmail.com>
2025-03-22 07:05:01 +0530

    Handled issue for large files sharing

--- src/test-client.js
+++ src/test-client.js
@@ -1,11 +1,21 @@
 const net = require('net');
 const FileSharingProtocol = require('./file-sharing-protocol');
 
-const HOST = '192.168.170.222';  
+const HOST = '192.168.170.222';
 const PORT = 3000;
-const FILE_PATH = './public/images.jpeg';  
+const FILE_PATH = './public/vision2.zip';
 
-const client = net.createConnection(PORT, HOST, () => {
+const client = net.createConnection(PORT, HOST, async () => {
     console.log('✅ Connected to server!');
-    FileSharingProtocol.sendFile(client, FILE_PATH);
+
+    await new Promise((resolve, reject) => {
+        try {
+            FileSharingProtocol.sendFile(client, FILE_PATH);
+            resolve(); // Mark as successful
+        } catch (err) {
+            reject(err); // Catch any error
+        }
+    });
+
+    console.log('✅ File Transferred Successfully');
 });
